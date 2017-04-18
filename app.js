8a6ffd578121f49c89cc27999a30287a1c92cb80
cyea <cyanskywing@outlook.com>
2017-04-18 13:38:06 +0800

    add express-session

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let app = express();
 
 init().then(function(config) {
   let router = require('./router/root.js');
+  let session = require('express-session');
+  app.use(session({
+    secret: 'just for test',
+    cookie: {maxAge: 24 * 60 * 60 * 1000}
+  }));
   app.use(router);
   app.listen(3000, function() {
       console.log('listen on 3000');
